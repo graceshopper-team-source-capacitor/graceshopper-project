f00286143e53a03db64ff7d34579c4e399bc6ee9
aliciahetrick <aliciahetrick@gmail.com>
2023-02-28 11:25:27 -0500

    Add product import to seed.js

--- script/seed.js
+++ script/seed.js
@@ -1,6 +1,9 @@
 'use strict'
 
-const {db, models: {User} } = require('../server/db')
+const {
+  db,
+  models: { User, Product },
+} = require('../server/db')
 
 /**
  * seed - this function clears the database, updates tables to
@@ -21,8 +24,8 @@ async function seed() {
   return {
     users: {
       cody: users[0],
-      murphy: users[1]
-    }
+      murphy: users[1],
+    },
   }
 }
 

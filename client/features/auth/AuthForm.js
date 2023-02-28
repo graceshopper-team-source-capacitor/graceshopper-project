31549e3f01c65b6e6f5e9fdb59e720cead971d5d
aliciahetrick <aliciahetrick@gmail.com>
2023-02-28 18:06:35 -0500

    Update seed to add magic method associations

--- client/features/auth/AuthForm.js
+++ client/features/auth/AuthForm.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { authenticate } from '../../app/store';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { authenticate } from '../../app/store'
 
 /**
   The AuthForm component can be used for Login or Sign Up.
   Props for Login: name="login", displayName="Login"
   Props for Sign up: name="signup", displayName="Sign Up"
 **/
-HEEE
+// HEEE
 const AuthForm = ({ name, displayName }) => {
-  const { error } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
+  const { error } = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
 
   const handleSubmit = (evt) => {
-    evt.preventDefault();
-    const formName = evt.target.name;
-    const username = evt.target.username.value;
-    const password = evt.target.password.value;
-    dispatch(authenticate({ username, password, method: formName }));
-  };
+    evt.preventDefault()
+    const formName = evt.target.name
+    const username = evt.target.username.value
+    const password = evt.target.password.value
+    dispatch(authenticate({ username, password, method: formName }))
+  }
 
   return (
     <div>
@@ -41,7 +41,7 @@ const AuthForm = ({ name, displayName }) => {
         {error && <div> {error} </div>}
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default AuthForm;
+export default AuthForm

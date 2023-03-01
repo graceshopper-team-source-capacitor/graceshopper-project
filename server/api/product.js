63ffbc5fe389233d3ba7534227aababc09242281
Jo Lee <jolee224@gmail.com>
2023-03-01 10:02:20 -0500

    all products path

--- server/api/product.js
+++ server/api/product.js
@@ -1,3 +1,15 @@
 const router = require('express').Router()
 const { models: { Product }} = require('../db')
 module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+      res.send(await Product.findAll({
+          // include: {
+          //     model: Tag
+          // }
+      }));
+  } catch (error) {
+      next(error);
+  }
+});

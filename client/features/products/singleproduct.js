77011c52cce470731afe0c52fae6731cfe84bead
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-01 13:14:00 -0500

    added add to cart btn, added amount buttons to increment/decrement amount of product added to cart

--- client/features/products/singleproduct.js
+++ client/features/products/singleproduct.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchSingleProductAsync, selectSingleProduct } from './singleProductSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams, Link, useNavigate } from 'react-router-dom';
@@ -6,21 +6,32 @@ import { useParams, Link, useNavigate } from 'react-router-dom';
 const Product = () => {
     const dispatch = useDispatch();
     const {id} = useParams();
-    const product = useSelector(selectSingleProduct); 
+    const product = useSelector(selectSingleProduct);
+    const [amount, setAmount] = useState(1);
 
     useEffect(() => {
         dispatch(fetchSingleProductAsync(id));
     }, [dispatch]);
 
-    console.log(product.imageUrl)
+    const subtractFromAmount = () => {
+        setAmount(amount -1);
+    };
+
+    const addToAmount = () => {
+        setAmount(amount +1);
+    };
+
     return (
         <div>
-            {/* <img src="/bakery1.jpg" /> */}
             <img src={`/${product.imageUrl}`} />
             <p>{product.name}</p>
             <p>${product.price}</p>
             <p>{product.type}</p>
             <p>{product.description}</p>
+                <button onClick={subtractFromAmount}>-</button>
+                <p>{amount}</p>
+                <button onClick={addToAmount}>+</button>
+            <button onClick={() => {console.log("add to cart btn clicked")}}>Add to Cart</button>
         </div>
     )
 }

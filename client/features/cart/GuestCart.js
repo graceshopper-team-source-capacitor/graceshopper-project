d254ad14b3b22c19311290cb57eeadd6307f002f
aliciahetrick <aliciahetrick@gmail.com>
2023-03-02 08:27:28 -0500

    Add total price to local cart

--- client/features/cart/GuestCart.js
+++ client/features/cart/GuestCart.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
+// TODO:
+// add to cart qty,
+// subtract from cart qty,
+// remove item from cart
+
 /**
  * COMPONENT
  */
@@ -21,6 +26,22 @@ const GuestCart = (props) => {
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(cart))
   }, [cart])
+
+  // TOTAL CART PRICE (START)
+  // creates an array of all prices in local cart
+  const cartPriceArray = []
+  for (let i = 0; i < cart.length; i++) {
+    cartPriceArray.push(cart[i].price * cart[i].qty)
+  }
+
+  // adds all the prices together
+  const initialValue = 0
+  const sumOfPrices = cartPriceArray.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  )
+  // TOTAL CART PRICE (END)
+
   return (
     <>
       <h3>Cart</h3>
@@ -33,6 +54,7 @@ const GuestCart = (props) => {
           </li>
         ))}
       </ul>
+      <h4>Total Price: ${sumOfPrices}</h4>
     </>
   )
 }

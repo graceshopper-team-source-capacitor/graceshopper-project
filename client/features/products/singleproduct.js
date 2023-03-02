8f55a66a012d366e31d4c1d50c6bbb794516f3b7
aliciahetrick <aliciahetrick@gmail.com>
2023-03-01 19:57:09 -0500

    Set state of local cart

--- client/features/products/singleproduct.js
+++ client/features/products/singleproduct.js
@@ -1,39 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import { fetchSingleProductAsync, selectSingleProduct } from './singleProductSlice';
-import { useSelector, useDispatch } from 'react-redux';
-import { useParams, Link, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { fetchSingleProductAsync, selectSingleProduct } from './singleProductSlice'
+import { useSelector, useDispatch } from 'react-redux'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 
 const Product = () => {
-    const dispatch = useDispatch();
-    const {id} = useParams();
-    const product = useSelector(selectSingleProduct);
-    const [amount, setAmount] = useState(1);
+  const dispatch = useDispatch()
+  const { id } = useParams()
+  const product = useSelector(selectSingleProduct)
 
-    useEffect(() => {
-        dispatch(fetchSingleProductAsync(id));
-    }, [dispatch]);
+  const [amount, setAmount] = useState(1)
+  const [cart, setCart] = useState([{ name: 'fakeItem' }])
 
-    const subtractFromAmount = () => {
-        setAmount(amount -1);
-    };
+  useEffect(() => {
+    dispatch(fetchSingleProductAsync(id))
+  }, [dispatch])
 
-    const addToAmount = () => {
-        setAmount(amount +1);
-    };
+  const subtractFromAmount = () => {
+    setAmount(amount - 1)
+  }
 
-    return (
-        <div>
-            <img src={`/${product.imageUrl}`} />
-            <p>{product.name}</p>
-            <p>${product.price}</p>
-            <p>{product.type}</p>
-            <p>{product.description}</p>
-                <button onClick={subtractFromAmount}>-</button>
-                <p>{amount}</p>
-                <button onClick={addToAmount}>+</button>
-            <button onClick={() => {console.log("add to cart btn clicked")}}>Add to Cart</button>
-        </div>
-    )
+  const addToAmount = () => {
+    setAmount(amount + 1)
+  }
+
+  const addToCart = () => {
+    const addedItem = { ...product, qty: amount }
+    const newCart = [...cart, addedItem]
+    setCart(newCart)
+  }
+
+  return (
+    <div>
+      <img src={`/${product.imageUrl}`} />
+      <p>{product.name}</p>
+      <p>${Number(product.price).toFixed(2)}</p>
+      <p>{product.type}</p>
+      <p>{product.description}</p>
+      <button onClick={subtractFromAmount}>-</button>
+      <p>{amount}</p>
+      <button onClick={addToAmount}>+</button>
+      <button onClick={addToCart}>Add to Cart</button>
+    </div>
+  )
 }
 
-export default Product 
+export default Product

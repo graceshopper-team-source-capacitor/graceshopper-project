7a4ef26b116123a8218c1d5476d5c53d0a127b13
aliciahetrick <aliciahetrick@gmail.com>
2023-03-02 20:29:23 -0500

    Add persistence to redux store

--- client/app/store.js
+++ client/app/store.js
@@ -4,17 +4,29 @@ import authReducer from '../features/auth/authSlice'
 import guestCartSlice from '../features/cart/guestCartSlice'
 import allProductReducer from '../features/products/allProductsSlice'
 import singleProductSlice from '../features/products/singleProductSlice'
-import UsersSlice from '../features/users/userListSlice';
+import UsersSlice from '../features/users/userListSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer } from 'redux-persist'
+import { combineReducers } from '@reduxjs/toolkit'
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const reducer = combineReducers({
+  auth: authReducer,
+  products: allProductReducer,
+  singleProduct: singleProductSlice,
+  guestCart: guestCartSlice,
+  users: UsersSlice,
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer)
 
 const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    products: allProductReducer,
-    singleProduct: singleProductSlice,
-    guestCart: guestCartSlice,
-    users: UsersSlice
-  },
-
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
 

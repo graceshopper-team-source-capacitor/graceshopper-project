2664b4e92b97bcf2c2ff582692c39be19b509ab7
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-05 11:56:08 -0500

    basic styling for homepage and navbar

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -1,46 +1,62 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
-import { logout } from '../../app/store'
-import { selectGuestCart } from '../cart/guestCartSlice'
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../../app/store";
+import { selectGuestCart } from "../cart/guestCartSlice";
 
 const Navbar = () => {
-  const isLoggedIn = useSelector((state) => !!state.auth.me.id)
-  const numItemsInCart = useSelector((state) => state.guestCart.numItemsInCart)
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
+  const numItemsInCart = useSelector((state) => state.guestCart.numItemsInCart);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const logoutAndRedirectHome = () => {
-    dispatch(logout())
-    navigate('/login')
-  }
+    dispatch(logout());
+    navigate("/login");
+  };
 
   return (
     <div>
-      <h1>FS-App-Template</h1>
       <nav>
         {isLoggedIn ? (
           <div>
             {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <button type="button" onClick={logoutAndRedirectHome}>
+            <Link to="/home" className="navLink">
+              Home
+            </Link>
+            <button
+              className="navLink"
+              type="button"
+              onClick={logoutAndRedirectHome}
+            >
               Logout
             </button>
-            <Link to="/products">Products</Link>
-            <Link to="/cart">Cart</Link>
+            <Link to="/products" className="navLink">
+              Products
+            </Link>
+            <Link to="/cart" className="navLink">
+              Cart
+            </Link>
           </div>
         ) : (
           <div>
             {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-            <Link to="/products">Products</Link>
-            <Link to="/cart">Cart({numItemsInCart})</Link>
+            <Link to="/login" className="navLink">
+              Login
+            </Link>
+            <Link to="/signup" className="navLink">
+              Sign Up
+            </Link>
+            <Link to="/products" className="navLink">
+              Products
+            </Link>
+            <Link to="/cart" className="navLink">
+              Cart({numItemsInCart})
+            </Link>
           </div>
         )}
       </nav>
-      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

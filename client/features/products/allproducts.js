9d3b99e419b804f86b4fe5b72bce50459a82b906
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-06 17:23:14 -0500

    side menu buttons styled for all products view

--- client/features/products/allproducts.js
+++ client/features/products/allproducts.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import { selectProducts } from './allProductsSlice'
-import { useSelector } from 'react-redux'
-import { NavLink, useNavigate, useLocation } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { fetchProductsAsync, deleteProductAsync } from './allProductsSlice'
-import { me } from '../../../client/app/store'
-import AuthForm from '../auth/AuthForm'
+import React, { useEffect, useState } from "react";
+import { selectProducts } from "./allProductsSlice";
+import { useSelector } from "react-redux";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchProductsAsync, deleteProductAsync } from "./allProductsSlice";
+import { me } from "../../../client/app/store";
+import AuthForm from "../auth/AuthForm";
 
 const ProductList = () => {
   // const location = useLocation()
-  const dispatch = useDispatch()
-  const products = useSelector(selectProducts)
-  const Navigate = useNavigate()
-  const loggedInAdmin = useSelector((state) => !!state.auth.me.isAdmin)
+  const dispatch = useDispatch();
+  const products = useSelector(selectProducts);
+  const Navigate = useNavigate();
+  const loggedInAdmin = useSelector((state) => !!state.auth.me.isAdmin);
   // console.log(useSelector((state) => state.auth.me.isAdmin));
 
   // const [category, setCategory] = useState("allCategories")
@@ -23,55 +23,76 @@ const ProductList = () => {
   // }) || [];
 
   useEffect(() => {
-    dispatch(fetchProductsAsync(products))
-  }, [dispatch])
+    dispatch(fetchProductsAsync(products));
+  }, [dispatch]);
 
   const handleDelete = async (id) => {
-    await dispatch(deleteProductAsync(id))
-    Navigate('/products')
-  }
+    await dispatch(deleteProductAsync(id));
+    Navigate("/products");
+  };
 
   // const handleEdit = async (id) => {
   //   await dispatch(editProductAsync(id))
   // }
 
   const handleFilter = async (event) => {
-    await dispatch(fetchProductsAsync({ type: event.target.value }))
-  }
-  
-  console.log("Product", products[0])
+    await dispatch(fetchProductsAsync({ type: event.target.value }));
+  };
+
+  console.log("Product", products[0]);
 
   return (
     <div className="productListParentDiv">
-      <label>Filter Category:</label>
-      <button onClick={handleFilter} value="all">
-        All
-      </button>
-      <button onClick={handleFilter} value="dairy">
-        Dairy
-      </button>
-      <button onClick={handleFilter} value="produce">
-        Produce
-      </button>
-      <button onClick={handleFilter} value="bakery">
-        Bakery
-      </button>
-      <button onClick={handleFilter} value="specialty">
-        Snacks
-      </button>
-      <h1 className="productListHeader">ALL PRODUCTS</h1>
-      {loggedInAdmin && (
-        <NavLink to={`/addproduct`} className="newProduct">
-          + Add New Product
-        </NavLink>
-      )}
+      <div className="sortSideMenu">
+        <h1 className="productListHeader">ALL PRODUCTS</h1>
+        {loggedInAdmin && (
+          <NavLink to={`/addproduct`} className="newProduct">
+            + Add New Product
+          </NavLink>
+        )}
+        <button className="sortByAllButton" onClick={handleFilter} value="all">
+          All
+        </button>
+        <button
+          className="sortByDairyButton"
+          onClick={handleFilter}
+          value="dairy"
+        >
+          Dairy
+        </button>
+        <button
+          className="sortByProduceButton"
+          onClick={handleFilter}
+          value="produce"
+        >
+          Produce
+        </button>
+        <button
+          className="sortByBakeryButton"
+          onClick={handleFilter}
+          value="bakery"
+        >
+          Bakery
+        </button>
+        <button
+          className="sortBySpecialtyButton"
+          onClick={handleFilter}
+          value="specialty"
+        >
+          Snacks
+        </button>
+      </div>
+
       <div id="productsList">
         {products.map((product) => (
           <div className="productInList" key={product.id}>
             <NavLink to={`/products/${product.id}`}>
               <img src={`/${product.imageUrl}`} className="productListImg" />
             </NavLink>
-            <NavLink to={`/products/${product.id}`} className="productNameProductList">
+            <NavLink
+              to={`/products/${product.id}`}
+              className="productNameProductList"
+            >
               <h2 className="productNameProductList">{product.name}</h2>
             </NavLink>
             <hr></hr>
@@ -82,7 +103,10 @@ const ProductList = () => {
                   <NavLink to={`/products/${product.id}/edit`}>
                     <button className="edit">Edit</button>
                   </NavLink>
-                  <button className="delete" onClick={() => handleDelete(product.id)}>
+                  <button
+                    className="delete"
+                    onClick={() => handleDelete(product.id)}
+                  >
                     Delete
                   </button>
                 </div>
@@ -94,7 +118,7 @@ const ProductList = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;

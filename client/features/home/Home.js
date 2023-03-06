727fb33627c47e64f7628fcab56d37c9b4129f6e
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-06 17:44:50 -0500

    added some more basic styling to homepage. needs to be completed

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -11,6 +11,24 @@ const Home = (props) => {
     <div className="homePageParentDiv">
       {/* <h3>Welcome, {username}</h3> */}
       <h1 className="homeTitleText">HOP MART</h1>
+      <div className="homeCategories">
+        <div className="homeBakery">
+          <img />
+          <p className="homeCategoryText">Bakery</p>
+        </div>
+        <div className="homeProduce">
+          <img />
+          <p className="homeCategoryText">Produce</p>
+        </div>
+        <div className="homeDairy">
+          <img />
+          <p className="homeCategoryText">Dairy</p>
+        </div>
+        <div className="homeSpecialty">
+          <img />
+          <p className="homeCategoryText">Snacks</p>
+        </div>
+      </div>
     </div>
   );
 };

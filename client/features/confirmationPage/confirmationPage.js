38765b743750fb725bfa706dd2702e7b40b39f06
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-07 12:32:38 -0500

    added confirmation page route to appRoutes.js so that it could be accessed by a guest user

--- client/features/confirmationPage/confirmationPage.js
+++ client/features/confirmationPage/confirmationPage.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 // import { confirmationPage } from '../features/cart/cartSlice';
 
 const ConfirmationPage = () => {
-    const max = 100000000000;
-    const min = 1000;
+  const max = 100000000000;
+  const min = 1000;
 
-    let orderNumber = Math.floor(Math.random() * (max - min + 1));
+  let orderNumber = Math.floor(Math.random() * (max - min + 1));
 
-    return (
-        <div className='confirmation-page'>
-            <h1>We've received your order!</h1>
-            <h1>Here are your order details </h1>
-            <h3>{`Order Number: #${orderNumber}`}</h3>
-            <NavLink to='/products'>
+  return (
+    <div className="confirmation-page">
+      <h1>We've received your order!</h1>
+      <h1>Here are your order details </h1>
+      <h3>{`Order Number: #${orderNumber}`}</h3>
+      {/* <NavLink to='/products'>
                 <button>Back to Homepage</button>
-            </NavLink>
-        </div>
-    )
+            </NavLink> */}
+    </div>
+  );
 };
 
 export default ConfirmationPage;
-

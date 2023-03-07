111ed5916fb2ecfb68cdbe3dfbe4d0ba37e6e628
Alexandra Bunty <buntyalexandra@gmail.com>
2023-03-07 09:58:46 -0500

    starting styling the guest cart view

--- client/features/cart/GuestCart.js
+++ client/features/cart/GuestCart.js
@@ -1,116 +1,128 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { increment } from '../cart/guestCartSlice'
-import { decrement } from '../cart/guestCartSlice'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { increment } from "../cart/guestCartSlice";
+import { decrement } from "../cart/guestCartSlice";
 
 /**
  * COMPONENT
  */
 const GuestCart = (props) => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const [cart, setCart] = useState([])
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [cart, setCart] = useState([]);
 
   // on first render, gets the cart saved in local storage
   // local storage persist on refresh
   useEffect(() => {
     try {
-      let localCart = localStorage.getItem('cart') || ''
+      let localCart = localStorage.getItem("cart") || "";
       // convert cart into json because local storage can only read strings & primative types
-      let jsonCart = JSON.parse(localCart)
-      if (localCart) setCart(jsonCart)
+      let jsonCart = JSON.parse(localCart);
+      if (localCart) setCart(jsonCart);
     } catch (err) {}
-  }, [])
+  }, []);
 
   // when cart updates set cart local storage
   useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart))
-  }, [cart])
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   // SUBTRACTING ITEM QTY IN CART (START)
   function subtractFromQty(index, cart) {
     // if the cart item quantiy is more than 1
     if (cart[index].qty > 1) {
       // subract one from the qty
-      cart[index].qty = cart[index].qty - 1
+      cart[index].qty = cart[index].qty - 1;
       // set the cart (with new item qty)
       // need to create a clone of cart since you cannot modify state directly
-      setCart(structuredClone(cart))
+      setCart(structuredClone(cart));
     } else if ((cart[index].qty = 1)) {
       //if the cart item qty is one, remove that item from the cart
-      removeFromCart(index, cart)
+      removeFromCart(index, cart);
     }
     // subtract one to the total number of items
     // needed to update navbar cart counter
-    dispatch(decrement())
+    dispatch(decrement());
   }
   // SUBTRACTING ITEM QTY IN CART (END)
 
   // ADDING ITEM QTY IN CART (START)
   function addToQty(index, cart) {
     // add one to that cart item's qty
-    cart[index].qty = cart[index].qty + 1
+    cart[index].qty = cart[index].qty + 1;
     // set the cart (with new item qty)
     // need to create a clone of cart since you cannot modify state directly
-    setCart(structuredClone(cart))
+    setCart(structuredClone(cart));
     // add one to the total number of items
     // needed to update navbar cart counter
-    dispatch(increment())
+    dispatch(increment());
   }
   // ADDING ITEM QTY IN CART (END)
 
   // REMOVING ITEM FROM CART (START)
   const removeFromCart = (index, cart) => {
     // delete one item at index from the cart
-    let splicedCart = cart.splice(index, 1)
+    let splicedCart = cart.splice(index, 1);
     // set the cart (not including the removed item)
     // need to create a clone of cart since you cannot modify state directly
-    setCart(structuredClone(cart))
-  }
+    setCart(structuredClone(cart));
+  };
   // REMOVING ITEM FROM CART (END)
 
   // TOTAL CART PRICE (START)
   // creates an array of all prices in local cart
-  const cartPriceArray = []
+  const cartPriceArray = [];
   for (let i = 0; i < cart.length; i++) {
-    cartPriceArray.push(cart[i].price * cart[i].qty)
+    cartPriceArray.push(cart[i].price * cart[i].qty);
   }
 
   // adds all the prices together
-  const initialValue = 0
+  const initialValue = 0;
   const sumOfPrices = cartPriceArray.reduce(
     (previousValue, currentValue) => previousValue + currentValue,
     initialValue
-  )
+  );
   // TOTAL CART PRICE (END)
 
   function handleCheckoutButton() {
-    navigate('/confirm')
+    navigate("/confirm");
   }
 
   return (
-    <>
+    <div className="cartParentDiv">
       <h3>Cart</h3>
-      <ul>
-        {cart.map((item, index) => (
-          <li key={item.id}>
-            <h4>{item.name}</h4>
-            <img src={item.imageUrl} />
-            <div>
-              <button onClick={() => subtractFromQty(index, cart)}>-</button>
-              <h4>Quantity: {item.qty}</h4>
-              <button onClick={() => addToQty(index, cart)}>+</button>
+      {cart.map((item, index) => (
+        <div className="cartProductParentDiv" key={item.id}>
+          <img src={item.imageUrl} />
+          <div className="cartProductInfo">
+            <h4 className="productNameSingleProductPurple">{item.name}</h4>
+            <h4 className="productPricePurple">
+              Total Price: ${Number(item.qty * item.price).toFixed(2)}
+            </h4>
+            <div className="qtyButtonPurple">
+              <button
+                className="qtyButtonPlusMinusPurple"
+                onClick={() => subtractFromQty(index, cart)}
+              >
+                -
+              </button>
+              <h4 className="qtyTextPurple">{item.qty}</h4>
+              <button
+                className="qtyButtonPlusMinusPurple"
+                onClick={() => addToQty(index, cart)}
+              >
+                +
+              </button>
             </div>
-            <h4>Price: ${Number(item.qty * item.price).toFixed(2)}</h4>
             <button onClick={() => removeFromCart(index, cart)}>Remove</button>
-          </li>
-        ))}
-      </ul>
+          </div>
+        </div>
+      ))}
       <h4>Total Price: ${sumOfPrices.toFixed(2)}</h4>
       <button onClick={handleCheckoutButton}>Checkout</button>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default GuestCart
+export default GuestCart;

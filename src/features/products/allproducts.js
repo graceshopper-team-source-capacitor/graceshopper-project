101e67e04a037254080cf0b6c57377ae2f112c82
aliciahetrick <aliciahetrick@gmail.com>
2023-03-07 12:20:53 -0500

    Logged in user can add one product to cart from all products view

--- src/features/products/allproducts.js
+++ src/features/products/allproducts.js
@@ -1,18 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { selectProducts } from "./allProductsSlice";
-import { useSelector } from "react-redux";
-import { NavLink, useNavigate, useLocation } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { fetchProductsAsync, deleteProductAsync } from "./allProductsSlice";
-import { me } from "../../app/store";
-import AuthForm from "../auth/AuthForm";
+import React, { useEffect, useState } from 'react'
+import { selectProducts } from './allProductsSlice'
+import { useSelector } from 'react-redux'
+import { NavLink, useNavigate, useLocation } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { fetchProductsAsync, deleteProductAsync } from './allProductsSlice'
+import { me } from '../../app/store'
+import AuthForm from '../auth/AuthForm'
+import { addLineItemForUserCart, addOneToLineItemQty, selectCart } from '../cart/userCartSlice'
 
 const ProductList = () => {
   // const location = useLocation()
-  const dispatch = useDispatch();
-  const products = useSelector(selectProducts);
-  const Navigate = useNavigate();
-  const loggedInAdmin = useSelector((state) => !!state.auth.me.isAdmin);
+  const dispatch = useDispatch()
+  const products = useSelector(selectProducts)
+  const Navigate = useNavigate()
+  const fetchedCart = useSelector(selectCart)
+  const loggedInAdmin = useSelector((state) => !!state.auth.me.isAdmin)
+  const me = useSelector((state) => state.auth.me)
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id)
+  const [amount, setAmount] = useState(1)
   // console.log(useSelector((state) => state.auth.me.isAdmin));
 
   // const [category, setCategory] = useState("allCategories")
@@ -23,23 +28,50 @@ const ProductList = () => {
   // }) || [];
 
   useEffect(() => {
-    dispatch(fetchProductsAsync(products));
-  }, [dispatch]);
+    dispatch(fetchProductsAsync(products))
+  }, [dispatch])
 
   const handleDelete = async (id) => {
-    await dispatch(deleteProductAsync(id));
-    Navigate("/products");
-  };
+    await dispatch(deleteProductAsync(id))
+    Navigate('/products')
+  }
 
   // const handleEdit = async (id) => {
   //   await dispatch(editProductAsync(id))
   // }
 
   const handleFilter = async (event) => {
-    await dispatch(fetchProductsAsync({ type: event.target.value }));
-  };
+    await dispatch(fetchProductsAsync({ type: event.target.value }))
+  }
 
-  console.log("Product", products[0]);
+  console.log('Product', products[0])
+
+  function lineItemProductIdsArrayFunc() {
+    const productIdArr = []
+    for (let i = 0; i < fetchedCart.lineItems?.length; i++) {
+      productIdArr.push(fetchedCart.lineItems[i].productId)
+    }
+    return productIdArr
+  }
+
+  const lineItemProductIdsArray = lineItemProductIdsArrayFunc()
+
+  function loggedInAdd(id) {
+    console.log('logged in added')
+    if (lineItemProductIdsArray.includes(id)) {
+      if (amount === 1) {
+        dispatch(addOneToLineItemQty({ userId: me.id, productId: id, amount }))
+      }
+    } else {
+      //if there's not already a line item, create a line item with the qty
+      dispatch(addLineItemForUserCart({ userId: me.id, productId: id, amount }))
+      console.log('fetched cart', fetchedCart)
+    }
+  }
+
+  function guestAdd() {
+    console.log('guest added')
+  }
 
   return (
     <div className="productListParentDiv">
@@ -53,32 +85,16 @@ const ProductList = () => {
         <button className="sortByAllButton" onClick={handleFilter} value="all">
           All
         </button>
-        <button
-          className="sortByDairyButton"
-          onClick={handleFilter}
-          value="dairy"
-        >
+        <button className="sortByDairyButton" onClick={handleFilter} value="dairy">
           Dairy
         </button>
-        <button
-          className="sortByProduceButton"
-          onClick={handleFilter}
-          value="produce"
-        >
+        <button className="sortByProduceButton" onClick={handleFilter} value="produce">
           Produce
         </button>
-        <button
-          className="sortByBakeryButton"
-          onClick={handleFilter}
-          value="bakery"
-        >
+        <button className="sortByBakeryButton" onClick={handleFilter} value="bakery">
           Bakery
         </button>
-        <button
-          className="sortBySpecialtyButton"
-          onClick={handleFilter}
-          value="specialty"
-        >
+        <button className="sortBySpecialtyButton" onClick={handleFilter} value="specialty">
           Snacks
         </button>
       </div>
@@ -89,12 +105,14 @@ const ProductList = () => {
             <NavLink to={`/products/${product.id}`}>
               <img src={`/${product.imageUrl}`} className="productListImg" />
             </NavLink>
-            <NavLink
-              to={`/products/${product.id}`}
-              className="productNameProductList"
-            >
+            <NavLink to={`/products/${product.id}`} className="productNameProductList">
               <h2 className="productNameProductList">{product.name}</h2>
             </NavLink>
+            {isLoggedIn ? (
+              <button onClick={() => loggedInAdd(product.id)}>Add One</button>
+            ) : (
+              <button onClick={() => guestAdd()}>Add One</button>
+            )}
             <hr></hr>
             {/* vvv These buttons need to be exclusively for the admin. vvv */}
             {loggedInAdmin && (
@@ -103,10 +121,7 @@ const ProductList = () => {
                   <NavLink to={`/products/${product.id}/edit`}>
                     <button className="edit">Edit</button>
                   </NavLink>
-                  <button
-                    className="delete"
-                    onClick={() => handleDelete(product.id)}
-                  >
+                  <button className="delete" onClick={() => handleDelete(product.id)}>
                     Delete
                   </button>
                 </div>
@@ -118,7 +133,7 @@ const ProductList = () => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProductList;
+export default ProductList
